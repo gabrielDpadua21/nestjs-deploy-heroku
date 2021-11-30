7a0b873d7e8396693b905192be26f952cf884f0e
Gabriel D. Padua <gabriel.padua@eduzz.com>
2021-11-30 01:20:22 -0300

    🧪test(file): fix unit test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe(
+      expect(appController.getHello().message).toBe(
         'Test deploy vercel with actions 3!!!',
       );
     });
